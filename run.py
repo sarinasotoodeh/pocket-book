4834d65da1e411ca54dd23bb07e830a8f3eca568
sarinasotoodeh <sarina.sotoodeh@yahoo.com>
2026-03-21 19:28:23 -0400

    database

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
-from flask import Flask, send_file, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
+import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = "database.db"
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route("/")
 def home():
-    # return "Home Page"
     return render_template("index.html")
 
 @app.route("/map.html")
@@ -27,19 +34,38 @@ def map_wasm():
 def map_pck():
     return send_from_directory("static/godot", "map.pck")
 
-@app.route('/schedules.html')
-def schedules():
-    return render_template('schedules.html')
+@app.route("/schedules")
+def all_schedules():
+    conn = get_db_connection()
+
+    schedules = conn.execute("""
+        SELECT * FROM classes
+        ORDER BY building_name, room_number, day_of_week, start_time
+    """).fetchall()
+
+    conn.close()
+
+    return render_template("schedules.html", schedules=schedules, mode="all")
+
+@app.route("/my-schedule/<int:student_id>")
+def my_schedule(student_id):
+    conn = get_db_connection()
+    # after log in is implemented: student_id = session["student_id"] 
+    schedules = conn.execute("""
+        SELECT c.*
+        FROM student_classes sc
+        JOIN classes c ON sc.class_id = c.class_id
+        WHERE sc.student_id = ?
+        ORDER BY day_of_week, start_time
+    """, (student_id,)).fetchall()
+
+    conn.close()
+
+    return render_template("schedules.html", schedules=schedules, mode="student")
 
 @app.route('/services.html')
 def services():
     return render_template('services.html')
-
-
-# @app.route("/about")
-# def about():
-#     # return "About Page"
-#     return send_file("index.html", name="")
 
 
 app.run()
