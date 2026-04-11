3a15cd8e894684e250d068413d1315a731752330
sarinasotoodeh <sarina.sotoodeh@yahoo.com>
2026-04-11 19:15:35 -0400

    f

--- run.py
+++ run.py
@@ -54,6 +54,7 @@ def all_schedules():
 @app.route("/my-schedule/<int:student_id>")
 def my_schedule(student_id):
     conn = get_db_connection()
+    # after log in is implemented: student_id = session["student_id"] 
     schedules = conn.execute("""
         SELECT c.*
         FROM student_classes sc
